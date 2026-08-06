ff8fdb4a432efa79dde3018c3be0f679f83c93a0
Lucas Soto <lucassoto877@gmail.com>
2026-08-06 20:06:06 -0300

    funciones principales

---
+++ script.js
@@ -0,0 +1,51 @@
+const form = document.getElementById('habito-form');
+const nombreHabito= document.getElementById('nombre-habito');
+const listaHabitos = document.getElementById('lista-habitos');
+
+class Habito{
+    constructor(nombre){
+        this.nombre = nombre;
+        this.id=Date.now().toString();
+        this.diasCompletados = [];
+
+    }
+}
+
+function getHabitos() {
+    const habitos = JSON.parse(localStorage.getItem('habitos')) || [];
+    return habitos;
+}
+
+function saveHabitos(habitos) {
+    const habito = getHabitos();
+    habitos.push(habito);
+    localStorage.setItem('habitos', JSON.stringify(habitos));
+}
+
+function mostrarHabitos() {
+    const habitos = getHabitos();
+    listaHabitos.innerHTML = habitos.map(habito => `
+        <div class="habito-card">
+            <h3>${habito.nombre}</h3>
+            <p class="racha">Racha: ${habito.diasCompletados.length} días</p>
+            <button onclick="eliminarHabito('${habito.id}')">Eliminar</button>
+        </div>
+    `).join('');
+}
+
+function eliminarHabito(id) {
+    let habitos = getHabitos();
+    habitos = habitos.filter(habito => habito.id !== id);
+    saveHabitos(habitos);
+    mostrarHabitos()
+    ;
+}
+
+//App principal
+
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const nuevoHabito = new Habito(nombreHabito.value);
+
+});
